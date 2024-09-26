c7201a068f6f757eb8c98167c55bf86dc4963488
deibcr <deibcr@gmail.com>
2024-09-26 10:15:37 -0400

    completed ShoppingList app 2

--- workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
@@ -1,4 +1,4 @@
-package com.pluralsight;
+package com.pluralsight.shoppinglist;
 
 public class ShoppingList {
 
@@ -6,7 +6,7 @@ public class ShoppingList {
         //// Create an array with 10 shopping items
         String[] shoppingItems ={
            "Chair","Desktop screen", "Mouse XP3", "Headset", "Portable Hard drive",
-           "CPU","Speakers", "Notepad", "Screen","USB external port"
+           "CPU","Speakers", "Notepad", "Portable Screen","USB external port"
         };
 
         //Print the shopping list
